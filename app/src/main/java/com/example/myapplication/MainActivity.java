e30bb31c69248d00ab9db08e52371b26a54b51aa
mahrous <mahrous@gmail.com>
2022-02-21 14:05:04 +0200

    first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.company.NetSDK.CB_fDisConnect;
+import com.company.NetSDK.INetSDK;
+import com.company.NetSDK.NET_PARAM;
+
+public class MainActivity extends AppCompatActivity {
+    Handler mHandler=new Handler();
+    boolean zRet=false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+// Prevent the failure of library loading.
+        INetSDK.LoadLibrarys();
+// Call the interface before using all SDK functions and set reconnection callback. When the network of
+        //     app and device disconnected, the callback will be triggered.
+// Only need to be called once
+        DeviceDisConnect mDisconnect = new DeviceDisConnect();
+         zRet = INetSDK.Init(mDisconnect);
+        if (!zRet) {
+            Log.e("TAG", "init NetSDK error!");
+            return;
+        }
+// Set the login network environment.
+        NET_PARAM stNetParam = new NET_PARAM();
+        stNetParam.nWaittime = 10; // Time out of common Interface.
+        stNetParam.nSearchRecordTime = 30; // Time out of Playback interface.
+        INetSDK.SetNetworkParam(stNetParam);
+
+
+    }
+
+    // When the network of app and device disconnected, the callback will be triggered. The detection time of
+//    disconnection is 60 seconds by default.
+    public class DeviceDisConnect implements CB_fDisConnect {
+        @Override
+        public void invoke(long loginHandle, String deviceIp, int devicePort) {
+            Log.d("TAG", "Device " + deviceIp + " is disConnected !");
+
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+// You can be notified here.
+// ToolKits.alertDisconnected();
+                }
+            });
+        }
+    }
+}
